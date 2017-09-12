53ff31a518341cb1566de3772c73861f7ea40296
Linda Chen <Linda@Lindas-MacBook-Pro.local>
2017-09-12 15:44:36 -0400

    You Are Awesome - Part 2

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -7,10 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var message:UILabel?
+    @IBOutlet weak var awesomeImage:UIImageView?
+    @IBOutlet weak var playSoundSwitch:UISwitch?
+    
+    var lastMessage = -1
+    var lastImage = -1
+    var lastSound = -1
+    var awesomePlayer = AVAudioPlayer()
+    
+    let messages = ["You Are Fantastic!",
+                    "You Are Great!",
+                    "You Are Amazing!",
+                    "Fabulous? That's You!",
+                    "You are the bomb!",
+                    "I love you."]
+    
+    let images = ["image0",
+                  "image1",
+                  "image2",
+                  "image3"]
+    
+    let sounds = ["sound0",
+                  "sound1",
+                  "sound2",
+                  "sound3",
+                  "sound4"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,25 +54,61 @@ class ViewController: UIViewController {
         return Int(arc4random_uniform(UInt32(max)))
     }
     
-    
-    @IBAction func buttonPressed( _ sender:UIButton){
-        
-        let messages = ["You Are Fantastic!",
-                        "You Are Great!",
-                        "You Are Amazing!",
-                        "Fabulous? That's You!",]
-        
-
-        var choice = -1
+    func RNG(lastNumber: Int, maxConstraint: Int)-> Int {
+        var newIndex = -1
         repeat {
-            choice = random(max:messages.count)
-        } while message?.text == messages[choice]
+            newIndex = Int(arc4random_uniform(UInt32(maxConstraint)))
+        } while lastNumber == newIndex
+        return newIndex
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
         
-        message?.text = messages[choice]
+        // Is it possible to load in the sound file?
+        if let sound = NSDataAsset(name: soundName) {
+            
+            // check if sound.data is a usable file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                
+                // if sound.data is not a valid audio file
+                print("ERROR: file \(soundName) didn't load.")
+            }
+        } else {
+            // if reading the assets folder didn't work
+            print("ERROR: file \(soundName) didn't load.")
+        }
         
     }
     
- 
+    @IBAction func switchChanged( _ sender:UISwitch){
+        if (!(playSoundSwitch?.isOn)! && lastSound != -1){
+                awesomePlayer.stop()
+        }
+    }
+    
+    
+    @IBAction func buttonPressed( _ sender:UIButton){
+
+        //Message
+        lastMessage = RNG(lastNumber: lastMessage, maxConstraint: messages.count)
+        message?.text = messages[lastMessage]
+        
+        //Image
+        lastImage = RNG(lastNumber: lastImage, maxConstraint: images.count)
+        awesomeImage?.image = UIImage(named: images[lastImage])
+        
+        //Pick Sound
+        lastSound = RNG(lastNumber: lastSound, maxConstraint: sounds.count)
+        
+        //Play sound
+        let soundName = sounds[lastSound]
+        if (playSoundSwitch?.isOn)! {
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+        }
+    }
 
 }
 
